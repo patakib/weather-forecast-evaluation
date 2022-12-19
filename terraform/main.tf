84f714d8fbc4a6cad4b5853c5fab3f77e724f2ba
Balint Pataki <pataki.balint@gmx.com>
2022-12-19 22:53:26 +0100

    changing resource details in terraform aws

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,9 +11,11 @@ provider "aws" {
 }
  
 resource "aws_ami_copy" "weather_test_instance" {
-  source_ami_id = "ami-0caef02b518350c8b"
+  name              = "terraform-example"
+  description       = "A copy of ami-0caef02b518350c8b"
+  source_ami_id     = "ami-0caef02b518350c8b"
   source_ami_region = "eu-central-1"
-  instance_type = "t2.micro"
+
   tags = {
     Name = "weather_test_instance"
   }
